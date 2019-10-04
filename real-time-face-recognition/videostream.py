d3da8b903e4f0d53a91335b1e5afb2d566f1006b
thearjun <mailarjunadhikari@gmail.com>
2019-10-05 01:33:36 +0545

    Basic VideoStream with Frame of Images

---
+++ real-time-face-recognition/videostream.py
@@ -0,0 +1,17 @@
+# Read a VideoStream from camera
+import cv2
+
+cap=cv2.VideoCapture(0)
+
+while True:
+	ret,frame=cap.read()
+	if ret==False:
+		continue
+	cv2.imshow("Frame",frame)
+	key_pressed=cv2.waitKey(1) & 0xFF
+	if key_pressed==ord('q'):
+		break
+cap.release()
+cv2.destroyAllWindows()
+
+
